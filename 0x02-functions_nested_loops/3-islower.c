ba47724a1f9baf6aec64c61132b4a127a2f1e279
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-04-12 13:52:35 +0300

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * islower - check if upper or lowercase
+ *
+ * Return: 1 if true and o otherwise
+ */
+int _islower(int c)
+{
+    if (islower(c))
+        return (1);
+    return (0);
+}
