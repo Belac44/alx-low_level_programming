be91ad0733d6bf09b5ad914d421090fcef57716b
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-04-12 12:48:56 +0300

    print -putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
-#include "_putchar.c"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - starting point
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: return 0 if successful
  */
 
 int main(void)
