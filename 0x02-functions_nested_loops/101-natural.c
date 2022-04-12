eb2ee7edd023aa84b558803e4a2157a093e7d717
Belac44 <87060914+Belac44@users.noreply.github.com>
2022-04-12 15:26:23 +0300

    added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i, total = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			total += i;
+	}
+	printf("%d\n", total);
+	return (0);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
